b0d23ee68e6acd7183f580f241b0c302998e9fb0
mmxlkkBenyapa <59160581@go.buu.ac.th>
2019-11-06 02:10:23 +0700

    on funtion

--- app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
+++ app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
@@ -152,4 +152,29 @@ class MainActivity : AppCompatActivity() {
         super.onStart()
         Timber.i("onCreate called")
     }
+
+    override fun onResume() {
+        super.onResume()
+        Timber.i("onResume Called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Timber.i("onPause Called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Timber.i("onStop Called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Timber.i("onDestroy Called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Timber.i("onRestart Called")
+    }
 }
